81f3d0a5873df7bdf06f431cfcc4d39a47decbe4
HSgod <hsgodkontakt@gmail.com>
2023-11-11 15:37:16 +0100

    Add option to add avatar
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,19 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
+const getImageFileType = require('../utils/getImageFileType');
 
 exports.register = async (req, res) => {
     try {
         const { login, password } = req.body;
-        if (login && typeof login === 'string' && password && typeof password === 'string') {
+        const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
+
+        if (login && typeof login === 'string' && password && typeof password === 'string' && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)) {
             const userWithLogin = await User.findOne({ login });
             if (userWithLogin) {
                 return res.status(409).send({ message: 'User with this login already exists' });
             }
-            const user = await User.create({ login, password: await bcrypt.hash(password, 10) });
-            res.status(201).send({ message: 'User created:' + '' + user.login });
+            const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar: req.file.filename });
+            res.status(201).send({ message: 'User created: ' + '' + user.login });
         } else {
             res.status(400).send({ message: "Bad request" });
         }
