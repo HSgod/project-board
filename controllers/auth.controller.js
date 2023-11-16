56a34583f9deffa2fb2b3b8cb64b3c0b228513ae
HSgod <hsgodkontakt@gmail.com>
2023-11-16 10:44:46 +0100

    Add ability to use cookie on development session
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,7 +35,7 @@ exports.login = async (req, res) => {
                 res.status(400).send({ message: 'Login or password are incorrect' });
             } else {
                 if (bcrypt.compareSync(password, user.password)) {
-                    req.session.login = user.login;
+                    req.session.user = { id: user._id, login: user.login };
                     res.status(200).send({ message: 'Login successful' });
                 } else {
                     res.status(400).send({ message: 'Login or password are incorrect' });
--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -1,5 +1,25 @@
+const Session = require('../models/Session.model');
 const authMiddleware = (req, res, next) => {
-    if (req.session.login) {
+    
+    if (process.env.NODE_ENV !== "production") {
+        Session.findOne().then((session, err) => {
+            if (err) {
+                res.status(401).send({ message: 'You are not authorized' });
+            }
+            else {
+                const sessionData = JSON.parse(session.session);
+                const user = {
+                    id: sessionData.user.id,
+                    login: sessionData.user.login,
+                }
+
+                req.session.user = user;
+                return next();
+            }
+        })
+    }
+
+    if (req.session.user) {
         next();
     } else {
         res.status(401).send({ message: 'You are not authorized' });
